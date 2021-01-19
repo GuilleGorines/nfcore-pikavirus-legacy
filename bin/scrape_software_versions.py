4675f2f3c42fe1aeb1703937a5bf39eafa9da79b
GuillermoGorines <60110848+GuillermoGorines@users.noreply.github.com>
2021-01-19 16:44:44 +0100

    Trimmomatic and Kraken2 added to step 0 .py file

--- bin/scrape_software_versions.py
+++ bin/scrape_software_versions.py
@@ -7,14 +7,19 @@ import re
 regexes = {
     "nf-core/pikavirus": ["v_pipeline.txt", r"(\S+)"],
     "Nextflow": ["v_nextflow.txt", r"(\S+)"],
-    "FastQC": ["v_fastqc.txt", r"FastQC v(\S+)"],
-    "MultiQC": ["v_multiqc.txt", r"multiqc, version (\S+)"],
+    "FastQC": ["v_fastqc.txt", r"FastQC version (\S+)"],
+    "MultiQC": ["v_multiqc.txt", r"multiqc version (\S+)"],
+    "Trimmomatic": ["v_trimmomatic.txt",r"Trimmomatic version (\S+)"],
+    "Kraken2": ["v_kraken2.txt", r"Kraken2 version (\S+)"],
 }
+
 results = OrderedDict()
 results["nf-core/pikavirus"] = '<span style="color:#999999;">N/A</span>'
 results["Nextflow"] = '<span style="color:#999999;">N/A</span>'
 results["FastQC"] = '<span style="color:#999999;">N/A</span>'
 results["MultiQC"] = '<span style="color:#999999;">N/A</span>'
+results["Trimmomatic"] = '<span style="color:#999999;">N/A</span>'
+results["Kraken2"] = '<span style="color:#999999;">N/A</span>'
 
 # Search each file using its regex
 for k, v in regexes.items():
