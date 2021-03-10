711a56adba925b508be43da8d911710018209a32
GuilleGorines <guillermo.gorines@gmail.com>
2021-03-10 12:33:30 +0100

    error when report is empty

--- bin/extract_reference_assemblies.py
+++ bin/extract_reference_assemblies.py
@@ -15,6 +15,9 @@ with open(krakenrep) as krakenfile:
     krakenfile = [line.split("\t") for line in krakenfile.readlines()]
     krakenfile = [col[4] for col in krakenfile if col[3]=="S"]
     taxid_list = set(krakenfile)
+    if len(taxid_list) == 0:
+        print(f"No IDs were found for {name_end}.")
+        sys.exit(2)    
 
 # Report:
 #   3: rank code (Unclass, Kingdom...)
@@ -25,9 +28,6 @@ with open(summary) as assembly_sum:
     assembly_sum = [line.split("\t") for line in assembly_sum.readlines() if not line.startswith("#")]
 
 assembly_sum = [[col[7],col[6],col[0],col[11],col[4],col[13],col[19]] for col in assembly_sum if col[6] in taxid_list]
-if len(assembly_sum) == 0:
-    sys.exit(1)
-
 raw_assembly = [[col[0],col[6]] for col in assembly_sum]
 
 # 0: assembly_accession
