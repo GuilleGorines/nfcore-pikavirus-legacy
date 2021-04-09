99e9bc262a0080044a6bbedfe16778a8831f1642
GuillermoGorines <60110848+GuillermoGorines@users.noreply.github.com>
2021-04-09 16:24:42 +0200

    graphs coverage is now python

--- bin/graphs_coverage.py
+++ bin/graphs_coverage.py
@@ -1,79 +1,90 @@
 #!/usr/bin/python
 
+# USAGE:
+#
+#  graphs_coverage.py Samplename coveragefiles
+# 
+# Calculates basic coverage statistics for coverage files provided. Samplename needed for file naming.
+#
+# This script has been developed exclusively for nf-core/pikavirus, and we cannot
+# assure its functioning in any other context. However, feel free to use any part
+# of it if desired.
+
+
 # Imports
 import sys
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import statistics
-
-# Needed msgs (errors)
-exitmsg1="\n2 files needed: blasn_filtered and coverage_table \n\n\
-USAGE: graphs_coverage.py \n"
-
-# Argument check
-if len(sys.argv) != 3:
-    sys.exit(exitmsg1)
-
-sampleCoverageDir = sys.argv[1]
-sampleName = sys.argv[2]
-fileName = f"{sampleCoverageDir}/{sampleName}_genome_coverage.txt"
 
 
-try:
-    with open(fileName) as file:
-        file = [slot for slot in file.readlines().split("\t")]
-
-except IOError:
-    sys.exit()
-
-
-## HAY QUE ARREGLAR LO DEL CUMSUM, no se hace así
-cov=sorted(
-    ([[element[0], element[1], element[4], 1-cumsum(element[4])] for element in file]),
-    key=lambda x: (x[1],x[2])
-    )
-
-new_cov=[["gnm", "covMean", "covMin", "covMax", "covSD", "covMedian"]]
-
-for uniq_genome in [set([element[0] for element in cov].sort())]:
-    coincidences = [genome in cov if cov[0]== uniq_genome]
-        covMean = sum([grouped_genomes[1]*grouped_genomes[2] for grouped_genomes in coincidences])
-        covMin = min([grouped_genmaxCov=500 # Value chosen for aesthetic reasons
-omes[1] for grouped_genomes in coincidences])
-        covMax = max([grouped_genomes[1] for grouped_genomes in coincidences])
-        covSD = sum([grouped_genomes[1]*(grouped_genomes[2]-covMean)² for grouped_genomes in coincidences])
-        covMedian = statistics.median([grouped_genomes[1] for grouped_genomes in coincidences])
-        new_cov.append([uniq_genome,covMean, covMin, covMax, covSD, covMedian])
-
-summary_cov=[["gnm","x1-x4","x5-x9","x10-x19",">x20","total"]]
-
-cov_frac_threshold_by_name=[["covThreshold","fracAboveThreshold"]]
-
-for uniq_genome in [set([element[0] for element in cov].sort())]:
-    coincidences = [genome in cov if cov[0]== uniq_genome]
-    cov_threshold.append([element[4], element[] for element in coincidences])
-    # Me quedo con una lista que contiene los datos de lo que comparte gnm
-    y0=sum([ element[4] for element in coincidences if coincidences[2] => 1 and coincidences[2] < 5 ])
-    y1=sum([ element[4] for element in coincidences if coincidences[2] => 5 and coincidences[2] < 10 ])
-    y2=sum([ element[4] for element in coincidences if coincidences[2] => 10 and coincidences[2] < 20 ])
-    y3=sum([ element[4] for element in coincidences if coincidences[2] >= 20 ])
-    y4=y0+y1+y2+y3
-
-    summary_cov.append([y0,y1,y2,y3,y4])
-
-cov_def = [new_cov[i].extend(i for i in cov_threshold_by_sample_name).extend(summary_cov[i][1-5] for i in range(len(summary_cov)) if new_cov[i][1] != 0])
-
-
-maxCov=500 # Chosen for aesthetic reasons
-threshold_pass= [element for element in cov_def if element[1] != 0 and element[6] < maxCov]
-for genome in threshold_pass:
-    x_axis=[ element[6] for element in threshold_pass ]
-    y_axis=[ element[]*100 for element in threshold_pass ]
-    plt.plot(x_axis,y_axis)
-    plt.ylabel("Depht of coverage")
-    plt.xlabel("Percentage of coverage")
-    plt.title("Genome coverage")
-    plt.show()
-    filename=f'{element[0]}_coverage_graph.pdf'
-    plt.savefig(filename)
-
+# Needed functions
+def weighted_avg_and_std(df,values, weights):
+    average = np.average(df[values], weights=df[weights])
+    variance = np.average((df[values]-average)**2, weights=df[weights])
     
+    return (average, variance**0.5)
+
+def calculate_weighted_median(df, values, weights):
+    cumsum = df[weights].cumsum()
+    cutoff = df[weights].sum() / 2.
+    
+    return df[cumsum >= cutoff][values].iloc[0]
+
+dataframe_list = []
+
+for filename in sys.argv[2:]:
+    tmp_dataframe = pd.read_table(filename,sep="\t",header=None)
+    dataframe_list.append(tmp_dataframe)
+
+df = pd.concat(dataframe_list)
+
+df.columns=["gnm","covThreshold","fractionAtThisCoverage","genomeLength","diffFracBelowThreshold"]
+
+df["diffFracBelowThreshold_cumsum"] = df.groupby('gnm')['diffFracBelowThreshold'].transform(pd.Series.cumsum)
+df["diffFracAboveThreshold"] = 1 - df["diffFracBelowThreshold_cumsum"]
+df["diffFracAboveThreshold_percentage"] = df["diffFracAboveThreshold"]*100
+
+data = {"gnm":[],"covMean":[],"covMin":[],"covMax":[],"covSD":[],"covMedian":[],
+        "x1-x4":[],"x5-x10":[],"x10-x19":[],">x20":[],"total":[]}
+
+for name, df_grouped in df.groupby("gnm"):
+    mean, covsd = weighted_avg_and_std(df_grouped,"covThreshold","diffFracBelowThreshold")
+    
+    if mean == 0:
+        pass
+    
+    minimum = min(df_grouped["covThreshold"])
+    maximum = max(df_grouped["covThreshold"])
+    median = calculate_weighted_median(df_grouped,"covThreshold","diffFracBelowThreshold")
+    
+    data["gnm"].append(name)
+    data["covMean"].append(mean)
+    data["covMin"].append(minimum)
+    data["covMax"].append(maximum)
+    data["covSD"].append(covsd)
+    data["covMedian"].append(median)
+    
+    y0=df_grouped.diffFracBelowThreshold[(df_grouped["covThreshold"] >= 1) & (df_grouped["covThreshold"] < 5)].sum()
+    y1=df_grouped.diffFracBelowThreshold[(df_grouped["covThreshold"] >= 5) & (df_grouped["covThreshold"] < 10)].sum()
+    y2=df_grouped.diffFracBelowThreshold[(df_grouped["covThreshold"] >= 10) & (df_grouped["covThreshold"] < 20)].sum()
+    y3=df_grouped.diffFracBelowThreshold[(df_grouped["covThreshold"] >= 20)].sum()
+    y4=y0+y1+y2+y3
+    
+    data["x1-x4"].append(y0)
+    data["x5-x10"].append(y1)
+    data["x10-x19"].append(y2)
+    data[">x20"].append(y3)
+    data["total"].append(y4)
+    
+    plot = df_grouped.plot.line(x="covThreshold",
+                                y="diffFracAboveThreshold_percentage",
+                                title=name,
+                                xlabel="Coverage",
+                                ylabel="Frac Above Threshold (%)",
+                                legend=None)
+    
+    plt.savefig(f"{name}.pdf")
+
+    newcov = pd.DataFrame.from_dict(data)
+    newcov.to_csv(f"{sys.argv[1]}_coverage_table.csv")
