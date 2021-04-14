10aa8ec70604457820a04393c8b3c407c8a0a70b
GuilleGorines <guillermo.gorines@gmail.com>
2021-04-14 23:59:53 +0200

    merge quality stats now works?

--- bin/merge_quality_stats.py
+++ bin/merge_quality_stats.py
@@ -2,81 +2,48 @@
 
 import sys
 
+def table(datalist):
+	filenames = [i[1] for i in datalist]
+	print("<tr>")
+	print("<td>Filename</td>")
+	for element in filenames:
+		print(f"<td>{element}</td>")
+	print("</tr>")
 
-def table(dictionary):
-	for key,values in dictionary.items():
-		if values[0] == "paired":
-			for sublist in values[1:]:
-				filename = [i[0] for i in sublist]
-				print("<tr>")
-				print("<td>Filename</td>")
-				for element in filename:
-					print(f"<td>{element}</td>")
-				print("</tr>")
+	seq_len = [i[2] for i in datalist]
+	print("<tr>")
+	print("<td>Sequence length</td>")
+	for element in seq_len:
+		print(f"<td>{element}</td>")
+	print("</tr>")
 
-				seq_len = [i[1] for i in sublist]
-				print("<tr>")
-				print("<td>Sequence length</td>")
-				for element in seq_len:
-					print(f"<td>{element}</td>")
-				print("</tr>")
+	total_seq = [i[3] for i in datalist]
+	print("<tr>")
+	print("<td>Total Sequences</td>")
+	for element in total_seq:
+		print(f"<td>{element}</td>")
+	print("</tr>")
 
-				total_seq = [i[2] for i in sublist]
-				print("<tr>")
-				print("<td>Total Sequences</td>")
-				for element in total_seq:
-					print(f"<td>{element}</td>")
-				print("</tr>")
-
-				gc_content = [i[3] for i in sublist]
-				print("<tr>")
-				print("<td>%GC</td>")
-				for element in gc_content:
-					print(f"<td>{element}</td>")
-				print("</tr>")
-		else:
-			filename = [i[0] for i in values]
-			print("<tr>")
-			print("<td>Filename</td>")
-			for element in filename:
-				print(f"<td>{element}</td>")
-			print("</tr>")
-
-			seq_len = [i[1] for i in values]
-			print("<tr>")
-			print("<td>Sequence length</td>")
-			for element in seq_len:
-				print(f"<td>{element}</td>")
-			print("</tr>")
-
-			total_seq = [i[2] for i in values]
-			print("<tr>")
-			print("<td>Total Sequences</td>")
-			for element in total_seq:
-				print(f"<td>{element}</td>")
-			print("</tr>")
-
-			gc_content = [i[3] for i in values]
-			print("<tr>")
-			print("<td>%GC</td>")
-			for element in gc_content:
-				print(f"<td>{element}</td>")
-			print("</tr>")
+	gc_content = [i[4] for i in datalist]
+	print("<tr>")
+	print("<td>%GC</td>")
+	for element in gc_content:
+		print(f"<td>{element}</td>")
+	print("</tr>")
 		
-
 	return
 
 file = sys.argv[1]
 
-pre_dict = {}
-post_dict = {}
+pre_list = []
+post_list = []
 
 
 with open(file,"r") as infile:
-	infile = sorted(infile.readlines())
+	infile = infile.readlines()
 	infile = [element.replace("\n","") for element in infile]
 	infile = [element.split(",") for element in infile]
-
+	infile.sort(key=lambda x:(x[0],x[1]))
 
 
 # 0: prepost, 1: filename, 2: seqlen, 3: nseqs, 4: gc_content, 5: path
@@ -90,57 +57,39 @@ for sample in infile:
 	gc_content = sample[6]
 	path = sample[7]
 
-	if end == "single_end":
-		if prepost == "pre":
-			pre_dict[samplename] = [filename,seqlen,nseqs,gc_content,path]
+	if prepost == "pre":
+		pre_list.append([samplename,filename,seqlen,nseqs,gc_content,path])
 
-		if prepost == "post":
-			post_dict[samplename] = [filename,seqlen,nseqs,gc_content,path]
-	
-	else:
-		pre_dict[samplename] = ["paired"]
-		post_dict[samplename] = ["paired"]
-
-		if prepost == "pre":
-			pre_dict[samplename].append([filename,seqlen,nseqs,gc_content,path])
-
-		if prepost == "post":
-			post_dict[samplename].append([filename,seqlen,nseqs,gc_content,path])
+	if prepost == "post":
+			post_list.append([samplename,filename,seqlen,nseqs,gc_content,path])
 
 print("<table class='table'>\
 		 			<tr>\
             <thead>\
               <th>Sample</th>")
 
-col_number = 0
-for key,values in pre_dict.items():
+for data in pre_list:
 
-	if values[0] == "paired":
-		col_number += 2
-		print(f"<th>{key}</th>")
-		print(f"<th>{key}</th>")
-
-	else:
-		col_number += 1
-		print(f"<th>{key}</th>")
+	print(f"<th>{data[0]}</th>")
+	print(f"<th>{data[0]}</th>")
 
 print("</thead><tbody></tr>")
-print(f"<tr><td colspan='{col_number}' class='info'>Pre-Filter</td></tr>")
-table(pre_dict)
+print(f"<tr><td colspan='{len(pre_list)+1}' class='info'>Pre-Filter</td></tr>")
+table(pre_list)
 
-print(f"</tr><tr><td colspan='{len(post_dict.keys())+1}'class='info'>Post-Filter</td></tr>")
-table(post_dict)
+print(f"</tr><tr><td colspan='{len(post_list)+1}'class='info'>Post-Filter</td></tr>")
+table(post_list)
 
 print("</tbody></table></div><div><br>")
 
 print ("<p>Pre-Filter Reports:</p><ul>")
-for key in pre_dict.keys():
-	print(f"<li><a target='_blank' href='{pre_dict[key][-1]}'>{key}</a></li>")
+for data in pre_list:
+	print(f"<li><a target='_blank' href='{data[-1]}'>{data[0]}:{data[1]}</a></li>")
 print("</ul>")
 
 print("<p>Post-Filter Reports:</p><ul>")
-for key in post_dict.keys():
-	print(f"<li><a target='_blank' href='{post_dict[key][-1]}'>{key}</a></li>")
+for data in post_list:
+	print(f"<li><a target='_blank' href='{data[-1]}'>{data[0]}{data[1]}</a></li>")
 print("</ul>")
 
 
