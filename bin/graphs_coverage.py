24c0c12265d2535e31cb73a287fb60c6792d64a5
GuilleGorines <guillermo.gorines@gmail.com>
2021-05-10 10:04:49 +0200

    graphs coverage updated

--- bin/graphs_coverage.py
+++ bin/graphs_coverage.py
@@ -30,30 +30,69 @@ def calculate_weighted_median(df, values, weights):
     
     return df[cumsum >= cutoff][values].iloc[0]
 
-# args managent
 
+# args managent
 outfile_name=sys.argv[1]
-coverage_files=sys.argv[2:]
+species_data=sys.argv[2]
+
+coverage_files=[sys.argv[3:]]
+
+with open(species_data) as species_data:
+    species_data = species_data.readlines()
+
+species_data = [line.split("\t") for line in species_data]
+species_data = [[line[3], line[4], line[6].strip("\n")] for line in species_data]
+
+
+# Remove the extension of the file (so it matches the filename)
+extensions = [".fna.gz",".fna"]
+
+for item in species_data:
+    for extension in extensions:
+        if item[2].endswith(extension):
+            item[2]=item[2].replace(extension,"")
+
+for item in coverage_files:
+        
+    match_name_coverage = os.path.basename(item).split("_vs_")[0]
+
+    for name in species_data:
+        if name[2] == match_name_coverage:
+            species = name[0]
+            subspecies = name[1]
+
+            with open(item,"r") as infile:
+                infiledata = [line.strip("\n") for line in infile.readlines()]
+                infiledata = [line.split("\t") for line in infiledata]
+                                
+            for line in infiledata:
+                if line[0] == "genome":
+                    line[0] = f"{species}_{subspecies}_genome"
+
+            with open(item,"w") as outfile:
+                for line in infiledata:
+                    filedata ="\t".join(line)
+                    outfile.write(f"{filedata}\t{species}\t{subspecies}\n")
 
 dataframe_list = []
 
 for filename in coverage_files:
     tmp_dataframe = pd.read_csv(filename,sep="\t",header=None)
-    tmp_dataframe=tmp_dataframe[~tmp_dataframe[0].str.contains("genome", na=False)]
     dataframe_list.append(tmp_dataframe)
 
 df = pd.concat(dataframe_list)
 
-df.columns=["gnm","covThreshold","fractionAtThisCoverage","genomeLength","diffFracBelowThreshold"]
+df.columns=["gnm","covThreshold","fractionAtThisCoverage","genomeLength","diffFracBelowThreshold","Species","Subspecies"]
 
 df["diffFracBelowThreshold_cumsum"] = df.groupby('gnm')['diffFracBelowThreshold'].transform(pd.Series.cumsum)
 df["diffFracAboveThreshold"] = 1 - df["diffFracBelowThreshold_cumsum"]
 df["diffFracAboveThreshold_percentage"] = df["diffFracAboveThreshold"]*100
 
-data = {"gnm":[],"covMean":[],"covMin":[],"covMax":[],"covSD":[],"covMedian":[],
+data = {"gnm":[],"species":[],"subspecies":[],"covMean":[],"covMin":[],"covMax":[],"covSD":[],"covMedian":[],
         "x1-x4":[],"x5-x10":[],"x10-x19":[],">x20":[],"total":[]}
 
 for name, df_grouped in df.groupby("gnm"):
+
     mean, covsd = weighted_avg_and_std(df_grouped,"covThreshold","diffFracBelowThreshold")
     
     if mean == 0:
@@ -64,6 +103,8 @@ for name, df_grouped in df.groupby("gnm"):
     median = calculate_weighted_median(df_grouped,"covThreshold","diffFracBelowThreshold")
     
     data["gnm"].append(name)
+    data["species"].append("".join(set(df_grouped["Species"])))
+    data["subspecies"].append("".join(set(df_grouped["Subspecies"])))
     data["covMean"].append(mean)
     data["covMin"].append(minimum)
     data["covMax"].append(maximum)
@@ -94,5 +135,5 @@ for name, df_grouped in df.groupby("gnm"):
     plt.savefig(f"{name}.pdf")
     plt.close()
 
-    newcov = pd.DataFrame.from_dict(data)
-    newcov.to_csv(f"{outfile_name}_table.csv")
+newcov = pd.DataFrame.from_dict(data)
+newcov.to_csv(f"{outfile_name}_table.csv")
